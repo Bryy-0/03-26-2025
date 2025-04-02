3d5365eefae9647ead846ac4c2bad170274a542d
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-04-02 09:20:52 +0800

    04-02-2025

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.ConstrainedExecution;
 using System.Transactions;
 
 
@@ -6,7 +7,7 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main(string[] args)
+/*        static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Let's play Roblox");
@@ -22,9 +23,45 @@ namespace HelloWorld
                 ("Username is: " +  User);
             Console.WriteLine
                 ("Password is: " + Pass);
+        }*/
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pick any number from 1-5");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int day))
+            {
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine("Sunday");
+                        break;
+                    case 2:
+                        Console.WriteLine("Monday");
+                        break;
+                    case 3:
+                        Console.WriteLine("Tuesday");
+                        break;
+                    case 4:
+                        Console.WriteLine("Wednesday");
+                        break;
+                    case 5:
+                        Console.WriteLine("Thursday");
+                        break;
+                    default:
+                        Console.WriteLine("Number of day doesn't exist");
+                        break;
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Number of day doesn't exist");
+            }
+
         }
     }
 }
 
 //Line by line comment --- Ctrl + K, then Ctrl + C = Comment --- Ctrl + K, then Ctrl + U = Uncomment
-/*Multiple line comment --- Toggle Block Comment Ctrl + Shift + */
+/*Multiple line comment --- Toggle Block Comment Ctrl + Shift + / */
